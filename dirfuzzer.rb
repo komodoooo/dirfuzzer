fe20e0ef9e76520aa0883c508d53b449da560434
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-09-09 16:48:30 +0200

    Add files via upload

---
+++ dirfuzzer.rb
@@ -0,0 +1,30 @@
+require 'http'
+
+def fuzzer
+    begin
+        puts "\rlink:"
+        fuzz_option = gets.chomp
+        print fuzz_option
+        puts "\rselect a wordlist:"
+        wordlist_option = gets.chomp
+        print wordlist_option
+        wordlist = File.open(wordlist_option)
+        wordlist.each do |dir|
+            uri = "#{fuzz_option}/#{dir}/"
+            request = HTTP.get(uri)
+            print request.code
+            if request.code == 200
+                puts "\rdirectory open! '#{dir}'"
+            elsif request.code == 404
+                puts "\nscanning..."                       #directory closed
+            end
+        end
+    rescue Errno::ENOENT
+        puts "\rERROR: Select a valid wordlist"
+        return false
+    rescue HTTP::ConnectionError
+        puts "\rERROR: Select a valid link"
+        return false
+    end
+end
+print fuzzer
