18f12d1e39188a94976ea4a4dd6d463aa212f3cd
Lojacopsen <68278515+Lojacops@users.noreply.github.com>
2021-09-13 10:10:12 +0200

    Update dirfuzzer.rb

--- dirfuzzer.rb
+++ dirfuzzer.rb
@@ -10,15 +10,12 @@ def check(string)
         print "Invalid URL: #{a}"
     end
 end
-
 def log(dir)
-    
     log = File.new("Valid.log", 'a')
     log.write(dir+"\n")
     log.close
 end
-
-def main(ispPass)
+def main
     banner = "
     ▄▄                                                              
     ▀███▀▀▀██▄   ██         ▀███▀▀▀███                                           
@@ -29,36 +26,38 @@ def main(ispPass)
       ██    ▄██▀ ██   ██      ██       ██    ██   ███  ▄  ███  ▄ ██▄    ▄ ██     
     ▄████████▀ ▄████▄████▄  ▄████▄     ▀████▀███▄███████ ███████  ▀█████▀████▄   
                                                                                  
-                                                                    ~Lojacops       
+                                                                    ~Lojacops, Renna       
     {A wise man once said, remember to delete full log files...}
-                    
     "
     puts banner
-    
-    print "Select a target: "
-    target = gets.chomp
-    if check(target) == true
-            print "Select a wordlist: "
-            wordlist = gets.chomp
-            if File.file?(wordlist) == false
-                print "ERROR: Invalid wordlist, please try again."
-                return false
-            end
-            arr_stuff = Array(File.open(wordlist))
-            ohyes = arr_stuff.map {|x| x.chomp}
-            ohyes.each do |dir|
-                uri = "#{target}/#{dir}/"
-                req = HTTP.get(uri)
-                code = req.code
-                if code == 200
-                    puts "Valid Dir: \"#{dir}\" - Code: #{code}"
-                    log(dir)
+    begin
+        print "Select a target: "
+        target = gets.chomp
+        if check(target) == true
+                print "Select a wordlist: "
+                wordlist = gets.chomp
+                if File.file?(wordlist) == false
+                    print "ERROR: Invalid wordlist, please try again."
+                    return false
                 end
-                if code != 200
-                    puts "Invalid Dir: \"#{dir}\" - Code: #{code}"
+                arr_stuff = Array(File.open(wordlist))
+                ohyes = arr_stuff.map {|x| x.chomp}
+                ohyes.each do |dir|
+                    uri = "#{target}/#{dir}/"
+                    req = HTTP.get(uri)
+                    code = req.code
+                    if code == 200
+                        puts "\nValid Dir: \"#{dir}\" - Code: #{code} || saved on file Valid.log!"
+                        puts "\n"
+                        log(dir)
+                    elsif code != 200
+                        puts "Invalid Dir: \"#{dir}\" - Code: #{code}"
+                    end
                 end
-            end
+        end
+    rescue HTTP::ConnectionError
+        puts "ERROR: Enable to connect at the target, please select a valid site."
+        return false
     end
 end
-
-main()
+print main
